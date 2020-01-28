597c77b6205aac794eb2cd1b4f395d5433cb07bc
eightball <50410328+Aisaac23@users.noreply.github.com>
2020-01-28 00:35:16 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -6,12 +6,12 @@
     - string123: is the base string and should be all different characters, for example: abcdefghijklm...
     - k: is the size of the token, it should be up to 5 when base string is 26 (alphabet) and up to 6 characters when the base string is 10 (0-9 digits).
   
-- It generates a csv file or stores them in memory with no more than 412MB.
-- This is the first version of this tool so there are some improvements to be made and some validations to code.
+- It generates a csv file.
+- This is the second version of this tool so there are some improvements to be made and some validations to code.
 - It's written in pure C Standard so it should run properly both Windows and Unix archs.
 - If you have any doubts about the base string and token sizes just use this equation: 
 # n!/(n-r)! 
 
-Where n is the base string size and r is the token size. Anything greater than 20,971,529 will result in more than 400MB in memory or file size.
-- Maybe some day I'll update it for a more flexible version.
+Where n is the base string size and r is the token size. Then the result should be multiplied by r and that's an aproximate file size in Bytes. 
+
 
--- TokenTool.c
+++ TokenTool.c
@@ -1,221 +1,151 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define MAX_TOKEN_SIZE 5
-#define MAX_INPUT_SIZE 31
-#define ONE_M 1024*1024
-#define TWENTY 20
-
-void rotate(unsigned int newSize, unsigned char * token);
-unsigned long long factorial(unsigned int n);
-void tokenPermutations(unsigned int newSize, unsigned char * permutedToken);
-unsigned int stringSearch(char * str, char target);
+void rotate(unsigned int newLength, char * token);
+void tokenPermutations(unsigned int newLength, char * permutedToken);
 void tokenCombinations(char token[], char inputString[31]);
-unsigned char** prepareStorage(unsigned int tokenSize, unsigned int inputSize);
-unsigned long long calculateTokens(unsigned int tokenSize, unsigned int inputSize);
-int validateArguments(int argc, char * argv[]);
-void printToken(unsigned char *token);
-void visualizeTokens(unsigned int sizeBlock);
+void printToken(char *token);
 
-FILE *tokenFile;
-unsigned char **strg;
-int file;
-unsigned long long nTokens;
+int menu(char *argv[]);
+
+FILE *tokenFile = NULL;
+int save = false;
+
 
 int main(int argc, char * argv[])
 {	
 
-	unsigned char token[11];
-	unsigned int tokenSize, inputSize, blockSize;
-	if(argc <= 1)
+	char *token;
+	unsigned int tokenLength;
+	
+	if( menu(argv) != -1)
 	{
-		printf("\nTokenTool ver 1.0 by Aisaac.\n");
+		if(save)
+			tokenFile = fopen(argv[2], "a");
+		
+		tokenLength = atoi(argv[argc-1]);
+		
+		token = calloc( tokenLength, sizeof(char) );
+		strncpy(token, argv[argc-2], tokenLength*sizeof(char));
+		token[tokenLength] = '\0';
+
+		tokenCombinations(token, argv[argc-2]);
+		
+		if(save)
+			fclose(tokenFile);
 		return EXIT_SUCCESS;
-	}
-	else if( !validateArguments(argc, argv) )
-		return EXIT_FAILURE;
+	}	
 
-	if(file)
-		tokenFile = fopen(argv[1], "a+");
-
-	tokenSize = atoi(argv[argc-1]);
-	inputSize = strlen(argv[argc-2]);
-
-	strncpy(token, argv[argc-2], tokenSize*sizeof(unsigned char));
-	token[tokenSize] = '\0';
-
-	strg = prepareStorage(tokenSize, inputSize);
-
-	tokenCombinations(token, argv[argc-2]);
-	
-	if(!file)
-	{
-		if(inputSize > 20)
-			printf("More than %llu tokens have been generated. Please enter an amount to visualize them in blocks (1-300): ", calculateTokens(tokenSize, 20));
-		else
-			printf("%llu tokens have been generated. Please enter an amount to visualize them in blocks (1-300): ", calculateTokens(tokenSize, inputSize));
-		scanf("%d", &blockSize);
-		fflush(stdin);
-		visualizeTokens(blockSize);
-	}
-	else
-		printf("A csv file %s has been created in the same directory as this application.");
-	
-	if(file)
-		fclose(tokenFile);
-	else
-		free(strg);
-	
-	return EXIT_SUCCESS;
+	return EXIT_FAILURE;
 }
 
-void visualizeTokens(unsigned int sizeBlock)
+int menu(char *argv[])
 {
-	unsigned long long  c = 0;
-	printf("\n");
-	while(c < nTokens)
+	char opc[1];
+	opc[0] = argv[1][1];
+	
+	if(argv[1][0])
+		return -1;
+
+	switch(opc[0])
 	{
-		if(c > 0 && c%sizeBlock == 0)
-		{
-			printf("\n\n%llu tokens displayed so far, press enter to continue...\n", c);
-			getchar();
-		}
-		printf("%s,", strg[c]);
-		c++;
-	}
-	printf("\n");	
+		case 'p':
+			save = false;
+		break;
+
+		case 's':
+			save = true;
+		break;
+		
+		default:
+			printf("Invalid option.");
+			return -1;
+	};
+	return 1;
 }
 
-void printToken(unsigned char *token)
+void printToken(char *token)
 {
-	printf("%s\n", token);
-	if(file)
+	static unsigned long count;
+	if(save)
 	{
 		fprintf(tokenFile, token);
 		fprintf(tokenFile, ",");
+		if(count > 0 && count%100==0)
+		{
+			count = 1;
+			fprintf(tokenFile, "\n");
+		}
+		count++;
 	}
 	else
-		strcpy(strg[nTokens++], token);
+		printf("%s,", token);
 }
 
-unsigned long long calculateTokens(unsigned int tokenSize, unsigned int inputSize)
-{
-	return (inputSize <= 20) ? ( factorial(inputSize)/factorial(inputSize-tokenSize) ): (TWENTY*ONE_M);
-}
-
-unsigned char** prepareStorage(unsigned int tokenSize, unsigned int inputSize)
-{
-	unsigned long long ntokens = calculateTokens(tokenSize, inputSize), c = 0;
-	unsigned char** storage = (unsigned char**) calloc( ntokens, sizeof(unsigned char*) );
-	
-	while(c < ntokens)
-	{
-		storage[c] = (unsigned char*) calloc( tokenSize, sizeof(unsigned char) );
-		if (storage[c] == NULL)
-			perror("Memory cannot be allocated:");
-		c++;
-	}
-	return storage;
-}
-
-unsigned long long factorial(unsigned int n)
-{
-	return n == 0 ? 1 : ( n*factorial(n-1) );
-}
-
-void rotate(unsigned int newSize, unsigned char * token)
+void rotate(unsigned int newLength, char * token)
 {
 	int k, size = strlen(token);
-    int position = size - newSize;
-    char temp = token[position];
-	
-    for(k=position+1; k<size; k++)      
-        token[k-1] = token[k];
-    token[k-1] = temp;  
+    	int position = size - newLength;
+	char temp = token[position];
+
+	for(k=position+1; k<size; k++)      
+	token[k-1] = token[k];
+	token[k-1] = temp;  
 }
 
-void tokenPermutations(unsigned int newSize, unsigned char * permutedToken)
+void tokenPermutations(unsigned int newLength, char * permutedToken)
 {
-	if(newSize == 1)
+	if(newLength == 1)
 		return;
-	for(int a = 0; a<newSize; a++)
+	for(int a = 0; a<newLength; a++)
 	{
-		tokenPermutations(newSize-1, permutedToken);             
-        if(newSize==2) 
+		tokenPermutations(newLength-1, permutedToken);             
+        if(newLength==2) 
             printToken(permutedToken); 
-        rotate(newSize, permutedToken);		
+        rotate(newLength, permutedToken);		
 	}
 }
 
-unsigned int stringSearch(char * str, char target)
+void tokenCombinations(char *token, char *inputString)
 {
-	int pos = 0;
-	while( pos < strlen(str))
-		if(str[pos] == target)
-			return pos;
-		else
-			pos++;
-	return -1;
-}
-
-void tokenCombinations(char token[], char inputString[31])
-{
-	int tokenSize = strlen(token);
-	int range = tokenSize-2, repIndex = tokenSize-2, index = tokenSize-1, inputSize = strlen(inputString);
-	char permutedToken[11];
+	int tokenLength = strlen(token);
+	int range = tokenLength-2, nextTokenBeginAt = tokenLength-2, index = tokenLength-1, inputLength = strlen(inputString);
+	char *permutedToken = calloc( tokenLength+1, sizeof(char) ), *position, newPosition[inputLength];
 	
-	while(token[0] != inputString[(inputSize-1) - (tokenSize-1)] )
+	while(token[0] != inputString[(inputLength-1) - (tokenLength-1)] )
 	{
 		strcpy(permutedToken,token);
+		permutedToken[tokenLength] = '\0';
 		//printToken(token);
-		tokenPermutations(tokenSize, permutedToken);
+		tokenPermutations(tokenLength, permutedToken);
+
 		index++;
-		if(index == inputSize)
+		if(index == inputLength)
 		{
-			while(inputSize-range-1 == tokenSize-repIndex-1)
+			while(inputLength-range-1 == tokenLength-nextTokenBeginAt-1)
 			{
-				range = stringSearch(inputString, token[repIndex-1]);
-				repIndex--;
+				position = strchr(inputString, token[nextTokenBeginAt-1]);
+				sprintf(newPosition, "%ld", position-inputString);
+				range = atoi(newPosition);
+				nextTokenBeginAt--;
 			}
 			
 			range++;
-			index = range + (tokenSize -1 -repIndex);
+			index = range + (tokenLength -1 -nextTokenBeginAt);
 			int plus = 0;
 			
-			for(int a = repIndex; a<tokenSize; a++)
+			for(int a = nextTokenBeginAt; a<tokenLength; a++)
 				token[a] = inputString[range+plus++];
-			repIndex = tokenSize-2;
+			nextTokenBeginAt = tokenLength-2;
 			
-			range = stringSearch(inputString, token[repIndex]);
+			position = strchr(inputString, token[nextTokenBeginAt]);
+			sprintf(newPosition, "%ld", position-inputString );
+			range = atoi(newPosition); // we get the new value of range with the position
 		}
-		token[tokenSize-1] = inputString[index];
+		token[tokenLength-1] = inputString[index];
 	}
 	strcpy(permutedToken,token);
-	tokenPermutations(tokenSize, permutedToken);
-}
-
-int validateArguments(int argc, char * argv[])
-{
-	int tokenLength = atoi(argv[argc-1]);
-	file = (argc == 4) ? 1 : 0; 
-	
-	if(tokenLength > 10 || tokenLength < 3)
-		perror("Error: token size should be no less than 3 and no more than 10");
-	else if(strlen(argv[argc-2]) > 31 || strlen(argv[argc-2]) < 3 )
-		perror("Error: base string should be a string between 3 and 31 characters length");
-	else if( argc == 4 )
-	{
-		if( (tokenFile = fopen(argv[1], "a") )== NULL)
-			perror("Error trying to open file: ");
-		else
-		{
-			fclose(tokenFile);
-			return 1;
-		}
-	}
-	else
-		return 1;
-	
-	return 0;
+	tokenPermutations(tokenLength, permutedToken);
 }
